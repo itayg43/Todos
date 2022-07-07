94bd7c0e6a6f89179d465428f9c0df6ced89a195
itayg43 <93944494+itayg43@users.noreply.github.com>
2022-07-07 08:33:54 +0300

    edit: fetch all todos in index.js

--- client/src/pages/Todos/Todos-page.jsx
+++ client/src/pages/Todos/Todos-page.jsx
@@ -1,22 +1,13 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { Heading } from "monday-ui-react-core";
 
-import { fetchAllTodos } from "../../redux/todos/actions";
+import TodoSuccessToast from "./components/Todo-success-toast";
+import TodoErrorToast from "./components/Todo-error-toast";
 import TodoForm from "./components/Todo-form";
 import TodoList from "./components/todo-list/Todo-list";
 import TodosFiltersPanel from "./components/Todos-filters-panel";
 import styles from "./todos-page.module.css";
-import TodoSuccessToast from "./components/Todo-success-toast";
-import TodoErrorToast from "./components/Todo-error-toast";
 
 const TodosPage = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchAllTodos());
-  }, [dispatch]);
-
   return (
     <>
       <TodoSuccessToast />
